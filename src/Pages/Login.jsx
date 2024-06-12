1ef1fc8f8af3884130668b02cd883cd4c08ace31
Perry Aryee <47823480+perryaryee@users.noreply.github.com>
2024-06-12 12:21:44 +0000

    login-page

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className=' grid place-items-center'>
+            <div className=' mt-10'>
+                <div className=' space-y-4'>
+                    <h1 className=' text-center text-5xl'>Sign in</h1>
+                    <p className=' text-center text-[#5CB85C]'>Need an account?</p>
+                </div>
+                <div className=' space-y-6 pt-4 px-2 lg:px-0'>
+                    <input placeholder='Email' className=' outline-none py-4 px-5  border border-[#dddddd] w-full rounded-[5px]' />
+                    <input placeholder='Password' className=' outline-none py-4 px-5  border border-[#dddddd] w-full rounded-[5px]' />
+                    <input placeholder='Password' className=' outline-none py-4 px-5  border border-[#dddddd] w-full rounded-[5px]' />
+                    <div className=' flex justify-end py-2'>
+                        <button className=' bg-[#5CB85C] text-[white] py-4 text-xl px-8 rounded-[5px] flex'>Sign in</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
